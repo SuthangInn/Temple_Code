b999ffd3c6075f621efa732d57224484e82a0532
sophittha <smilinglypop@gmail.com>
2015-05-04 18:59:57 +0700

    Dhamma Knowledge (working add edit delete 100% view 50%)

--- TempleAll/TempleAll/Controllers/AdminController.cs
+++ TempleAll/TempleAll/Controllers/AdminController.cs
@@ -17,6 +17,11 @@ namespace TempleAll.Controllers
             return View();
         }
 
+        private TempledbEntities Historycontext = new TempledbEntities();
+        private KnowledgedbEntities Knowledgecontext = new KnowledgedbEntities();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public ActionResult History()
         {
             List<History> all = new List<History>();
@@ -29,12 +34,11 @@ namespace TempleAll.Controllers
             return View(all);
         }
 
-        public ActionResult AdminEditHistory(int id)
+        public ViewResult AdminEditHistory(int id)
         {
-            return View("AdminEditHistory",context.Histories.Find(id));
+            return View("AdminEditHistory", Historycontext.Histories.Find(id));
         }
 
-        private TempledbEntities context = new TempledbEntities();
         [HttpPost]
         public ActionResult AdminEditHistory(History IG)
         {
@@ -61,24 +65,68 @@ namespace TempleAll.Controllers
 
                 IG.ImageData = data;
 
+                Historycontext.Entry(IG).State = EntityState.Modified;
 
-                //using (TempledbEntities dc = new TempledbEntities())
-                //{
-                //if (IG.HistoryID == 0)
-                //{ 
-                //context.Histories.Add(IG);
-                //}
-                //else
-                //{
-                    context.Entry(IG).State = EntityState.Modified;
-                //}
-                //}
-            
-                context.SaveChanges();  
+                Historycontext.SaveChanges();  
             return RedirectToAction("History");
 
-            }
-
         }
-    }
+
+         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public ViewResult AdminKnowledge() 
+        {
+            List<Knowledge> all = new List<Knowledge>();
+
+            using (KnowledgedbEntities kdb = new KnowledgedbEntities())
+            {
+                all = kdb.Knowledges.ToList();
+            }
+            return View(all); 
+        }
+
+        public ViewResult AdminEditKnowledge(int id)
+        {
+            return View("AdminEditKnowledge", Knowledgecontext.Knowledges.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult AdminEditKnowledge(Knowledge k)
+        {
+            Knowledgecontext.Entry(k).State = EntityState.Modified;
+            Knowledgecontext.SaveChanges();
+
+            return RedirectToAction("AdminKnowledge");
+        }
+
+        public ViewResult CreateKnowledge()
+        {
+            return View("CreateKnowledge", new Knowledge());
+        }
+
+        [HttpPost]
+        public ActionResult CreateKnowledge(Knowledge k)
+        {
+            Knowledgecontext.Entry(k).State = EntityState.Added;
+
+            Knowledgecontext.SaveChanges();
+            return RedirectToAction("AdminKnowledge");
+        }
+
+        public ActionResult DeleteKnowledge(int id)
+        {
+            Knowledge k = Knowledgecontext.Knowledges.Find(id);
+            if (k == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Knowledgecontext.Knowledges.Remove(k);
+                Knowledgecontext.SaveChanges();
+            }
+            return RedirectToAction("AdminKnowledge");
+        }
+   }
+}
 
