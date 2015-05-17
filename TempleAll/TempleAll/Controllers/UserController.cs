3be63ef636d0e74b3edc9a63ec20bb0ec081f3c6
sophittha <smilinglypop@gmail.com>
2015-05-17 13:31:16 +0700

    contact user

---
+++ TempleAll/TempleAll/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempleAll.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        ContactTopicdbEntities contactTopiccontext = new ContactTopicdbEntities();
+        ContactdbEntities contactcontext = new ContactdbEntities();
+        AskdbEntities askcontext = new AskdbEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public ViewResult ContactTopic()
+        {
+            List<Contact> all = new List<Contact>();
+            using (ContactdbEntities cdb = new ContactdbEntities())
+            { all = cdb.Contacts.ToList(); }
+            return View(all);
+        }
+
+        public ViewResult CreateContactTopic()
+        {
+            return View("CreateContactTopic", new ContactTopic());
+        }
+
+        [HttpPost]
+        public ActionResult CreateContactTopic(ContactTopic c)
+        {
+            contactTopiccontext.Entry(c).State = EntityState.Added;
+
+            contactTopiccontext.SaveChanges();
+            return RedirectToAction("ContactTopic");
+        }
+	
+    }
+}
